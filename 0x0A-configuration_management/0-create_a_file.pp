7805c4655584a006158c04dbb8c644cd62e19164
katgzco <katgzco@gmail.com>
2021-08-13 20:34:06 -0500

    Style: alignament in arrows

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-# create a file in /tmp
+# Create a file in /tmp
 file {'/tmp/holberton' :
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
